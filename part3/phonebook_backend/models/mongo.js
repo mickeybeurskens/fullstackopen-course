62b04f8e6ab00217e6793d6d0d30ab0fd2890c08
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-09 19:18:41 +0100

    Add exercise 3.20

--- part3/phonebook_backend/models/mongo.js
+++ part3/phonebook_backend/models/mongo.js
@@ -19,8 +19,15 @@ const personSchema = new mongoose.Schema({
       required: true,
     },
     number:{
-      type: Number,
+      type: String,
       required: true,
+      minlength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}\-\d*$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
     },
     date: Date,
     id: String,
