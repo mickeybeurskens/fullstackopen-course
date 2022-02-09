c392720567fb8230aebec5d92a18a48c7fda9ef4
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-09 19:18:42 +0100

    Add exercise 4.20 and fix all tests

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRoute.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user',
     { username: 1, name: 1 })
@@ -18,7 +10,7 @@ blogsRoute.get('/', async (request, response) => {
 })
 
 blogsRoute.post('/', async (request, response) => {
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({
       error: 'token missing or invalid'
