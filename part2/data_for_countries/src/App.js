7017554aa30ba7ebb7ffbc173e11c52bcc78c94d
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-09 19:18:39 +0100

    Add exercise 2.14

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -6,6 +6,7 @@ const ShowWeather = ({countries, weatherInfo}) => {
     return <div>
       <h1>Weather in {countries[0].name.common}</h1>
       <p>temperature: {weatherInfo.main.temp}</p>
+      <img src={`http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`}/>
       <p>wind: {weatherInfo.wind.speed}</p>
     </div>
   }
