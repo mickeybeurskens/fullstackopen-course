cf15702d55333ad23a232ded844dbfc1dd9d4089
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-09 19:18:38 +0100

    Add exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,7 +3,20 @@ import React, { useState } from 'react'
 const Button = ({clickCallback, text}) =>
   <button onClick={clickCallback}>{text}</button>
 
+const Statistics = ({good, neutral, bad}) => {
+  const totalClicks = good + neutral + bad
+  return <>
+    <Result text="good" amount={good}/>
+    <Result text="neutral" amount={neutral}/>
+    <Result text="bad" amount={bad}/>
+    <Result text="all" amount={totalClicks}/>
+    <Result text="average" amount={good-bad}/>
+    <ResultPercent text="positive" amount={good/totalClicks}/>
+  </>
+}
+
 const Result = ({text, amount}) => <p>{text} {amount}</p>
+const ResultPercent = ({text, amount}) => <p>{text} {amount} %</p>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -18,9 +31,7 @@ const App = () => {
       <Button clickCallback={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button clickCallback={() => setBad(bad + 1)} text="bad"/>
       <h1>statistics</h1>
-      <Result text="good" amount={good}/>
-      <Result text="neutral" amount={neutral}/>
-      <Result text="bad" amount={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
