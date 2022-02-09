57dcc1241d07e3a7fa5f21ff48c7bf0c6382279d
mickeybeurskens <mickeybeurskens@gmail.com>
2022-02-09 19:18:39 +0100

    Add exercise 2.13

--- part2/course_information/src/App.js
+++ part2/course_information/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Course from './components/Course'
 
 const AllCourses = ({courses}) => {
--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const ShowSearchResult = ({countries}) => {
+  if (countries.length > 10){
+    return <p>Too many matches, be more specific</p>
+  } else if (countries.length > 1) {
+    return countries.map(c => <p key={c.name.common}>{c.name.common}</p>)
+  } else if (countries.length === 1){
+    const details = countries[0]
+    const languages = Object.values(details.languages)
+    return <div>
+    <h1>{details.name.common}</h1>
+    <p>capital: {details.capital}</p>
+    <p>population: {details.population}</p>
+    <h2>languages</h2>
+    <ul>
+      {languages.map(lan => <li key={lan}>{lan}</li>)}
+    </ul>
+    <img src={details.flags.png} alt="Country flag"/>
     </div>
-  );
+  }
+  return <p>No country found</p>
 }
 
-export default App;
+const App = () => {
+  const [country, setCountry] = useState('')
+  const [countryData, setAllCountries] = useState([])
+
+  const getCountry = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(responce => setAllCountries(responce.data))
+  }
+  useEffect(getCountry, [])
+
+  const changeCountry = (event) => setCountry(event.target.value)
+  const getFilteredCountries = () => {
+    if (country.length!==0){
+      return countryData.filter((entry) =>  entry.name.common.match(country))
+    } 
+    return countryData
+  }
+
+  return <div>
+    <p>Find countries: <input value={country} onChange={changeCountry}/></p>
+    <ShowSearchResult countries={getFilteredCountries()}/>
+  </div>
+}
+
+export default App
